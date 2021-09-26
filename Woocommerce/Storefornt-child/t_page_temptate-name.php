cca30d6031e432dcc3a4cbf9a6b2a940019a06ab
Yev <tjs_694@hotmail.com>
2021-09-26 17:55:09 +0200

    add file to make new page template

---
+++ Woocommerce/Storefornt-child/t_page_temptate-name.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * The template for displaying full width pages.
+ *
+ * Template Name: -Template name here-
+ * @package storefront
+ */
+
+get_header(); ?>
+<!---------- HTML code here---------- >
+<?php
+get_footer();
